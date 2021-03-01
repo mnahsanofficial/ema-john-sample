45d294173d9995539e21cfe4a0eb6b924e8da803
Nazmul Ahsan <mnahsanofficial@gmail.com>
2021-03-01 08:15:35 +0600

    shoping page done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart=props.cart;
+    //const totalPrice=cart.reduce((total,prd)=>total+prd.price,0 );
+    let totalPrice=0;
+    for(let i=0;i<cart.length;i++){
+        const product =cart[i];
+        totalPrice=totalPrice+product.price;
+    }
+    let shipping=0;
+    if(totalPrice>35){
+        shipping=0;
+    }
+    else if(totalPrice>15){
+        shipping=4.99;
+    }
+    else if(totalPrice>0){
+        shipping=12.99;
+    }
+    const tax=totalPrice/10;
+
+    const formatNumber=num=>{
+        const precision=num.toFixed(2);
+        return Number(precision);
+    }
+    
+    
+
+   return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items Ordered: {cart.length}</p>
+            <p>Product Price:{totalPrice}</p>
+            <p>ShippingCost:{shipping}</p>
+            <p>Tax+VAT:{formatNumber( tax)}</p>
+            <p>Total Price:{formatNumber( totalPrice+shipping+tax)}</p>
+        </div>
+    );
+};
+
+export default Cart;
